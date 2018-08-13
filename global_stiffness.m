42d038cdff9bd6ccac318e3fcd8ca64f680e437d
annasimmons <32113501+annasimmons@users.noreply.github.com>
2018-08-13 08:01:16 -0700

    Update global_stiffness.m

--- global_stiffness.m
+++ global_stiffness.m
@@ -4,15 +4,18 @@ function K = global_stiffness(conn, N, ne, dof, check)
 K = zeros(dof, dof);
 
 for ii=1:ne
+    L = sqrt((N(conn(ii,1),1)-N(conn(ii,2),1))^2 + (N(conn(ii,1),2)-N(conn(ii,2),2))^2);
     P1 = conn(ii, 1); % near end
     P2 = conn(ii, 2); % check coordinates of this (far end)
-    lx = (N(P2, 1)-N(P1, 1))/ne; % so you divide by ne twice ?
-    ly = (N(P2, 2)-N(P1, 2))/ne; 
+    lx = (N(P2, 1)-N(P1, 1))/L; % so you divide by ne twice ?
+    ly = (N(P2, 2)-N(P1, 2))/L; 
     k = [lx^2 lx*ly -lx^2 -lx*ly;
          lx*ly ly^2 -lx*ly -ly^2;
          -lx^2 -lx*ly lx^2 lx*ly;
-         -lx*ly -ly^2 lx*ly ly^2]/ne;
+         -lx*ly -ly^2 lx*ly ly^2]/L;
      sctr = check(ii,:);
     K(sctr, sctr) = K(sctr, sctr) + k;
 end
+
+
 
